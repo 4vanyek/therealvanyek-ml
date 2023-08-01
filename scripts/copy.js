990cbf25e96d6b5b838a23978a9ff893d400e76c
4vanyek <z232566755z@gmail.com>
2023-08-01 16:28:42 +0300

    большое обновление

---
+++ scripts/copy.js
@@ -0,0 +1,45 @@
+// use a class selector if available 
+let blocks = document.querySelectorAll("precopy");
+
+blocks.forEach(block => {
+
+  // only add button if browser supports Clipboard API
+  if (navigator.clipboard) {
+
+    let button = document.createElement("button");
+	button.classList.add("copy-button");
+    
+    button.innerHTML = `<i class="fa-solid fa-copy"></i>&nbsp;Скопировать`;
+    
+    block.appendChild(button);
+
+    button.addEventListener("click", async () => {
+    
+      await copyCode(block);
+      
+    });
+
+  }
+
+});
+
+async function copyCode(block) {
+
+	const copyIconClass = "fa-solid fa-copy";
+	const copiedIconClass = "fa-solid fa-check";
+  
+	let code = block.querySelector("pre");
+	let text = code.innerText;
+  
+	await navigator.clipboard.writeText(text);
+  
+	// get button reference
+	let button = block.querySelector("button");
+  
+	button.innerHTML = `<i class="${copiedIconClass}"></i>&nbsp;Скопировано!`;
+  
+	setTimeout(() => {
+	  button.innerHTML = `<i class="${copyIconClass}"></i>&nbsp;Скопировать`;
+	}, 1000);
+  
+  }
